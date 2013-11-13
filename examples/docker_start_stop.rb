a745ce032d0d0c606801e71c3db48a3098441a20
Joe Williams <joe.williams@github.com>
2013-11-12 18:35:52 -0800

    add little tester script

---
+++ examples/docker_start_stop.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+## this is a crappy little tester script
+##
+## run zookeeper, docker and drank on the same machine
+##
+## run this script and you should see drank consume
+## docker data and send it to zk, killing sessions when
+## containers die
+
+loop do
+
+  start_count = 5
+  stop_count = 5
+
+  start_count.times do
+    out = `sudo docker run -d -e -i -t ubuntu /bin/bash`
+    puts "starting: #{out}"
+    sleep 1
+  end
+
+  containers=`docker ps | awk '{print $1}' | grep -v CONTAINER | tail -n #{stop_count}`.split
+
+  containers.each do |cont|
+    out = `docker stop #{cont}`
+    puts "stopping: #{out}"
+  end
+
+end
